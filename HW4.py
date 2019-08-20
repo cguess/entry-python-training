f316b677be5679ed35eb8c5967705354e71270f9
Arianna Soldati <ariannasoldati@Ariannas-MBP.fritz.box>
2019-08-21 00:54:28 +0200

    HW4.py

---
+++ HW4.py
@@ -0,0 +1,8 @@
+entry1 = {'headword': 'cannon', 'definition': 'stroke in which the cue ball strikes two balls successively'}
+entry2 = {'headword': 'gun', 'definition': 'well-developed biceps muscle'}
+entry3 = {'headword': 'tank', 'definition': 'cell in a police station or jail'}
+
+entry_list = [entry1, entry2, entry3]
+
+for entry_list in entry_list:
+  print(entry_list['headword'])
